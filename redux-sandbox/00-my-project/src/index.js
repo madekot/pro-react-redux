958bcb23917524a7772b3f820c5cb11ac4971d0b
Михаил Голубцов <mgolubcov@gmail.com>
2021-11-11 18:50:08 +0300

    w&p: 122. Action Creator

--- redux-sandbox/00-my-project/src/index.js
+++ redux-sandbox/00-my-project/src/index.js
@@ -4,8 +4,13 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case "INC":
       return state + 1;
+      
+    case "RND":
+      return state + action.payload;
+
     case "DEC":
       return state - 1;
+      
     default:
       return state
   }
@@ -23,6 +28,16 @@ document
   .getElementById('dec')
   .addEventListener('click', () => {
     store.dispatch({type: 'DEC'});
+  });
+  
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random()*10);
+    store.dispatch({
+      type: 'RND',
+      payload
+    });
   });
 
 const update = () => {
