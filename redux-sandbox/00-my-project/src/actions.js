7611240ab0d7eaea48857019d618b1bfb248e7b1
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-11-12 23:23:02 +0300

    fin: 128. mapDispatchToProps в виде объекта

--- redux-sandbox/00-my-project/src/actions.js
+++ redux-sandbox/00-my-project/src/actions.js
@@ -1,5 +1,10 @@
 const inc = () => ({ type: 'INC' });
 const dec = () => ({ type: 'DEC' });
-const rnd = (payload) => ({ type: 'RND', payload });
+const rnd = (payload) => {
+  return { 
+    type: 'RND', 
+    payload: Math.floor(Math.random()*10),
+  }
+};
 
 export {inc, dec, rnd}
