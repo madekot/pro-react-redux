b2fcac9eb53f7560fa7749a02f7cea2efedfca5a
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-11-13 19:37:53 +0300

    fin: 136. Чтение данных из Redux Store

--- re-store/00-my-project/src/components/book-list/book-list.js
+++ re-store/00-my-project/src/components/book-list/book-list.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import BookListItem from "../book-list-item";
+import { connect } from "react-redux";
 
 import './book-list.css';
 
@@ -18,4 +19,10 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+const mapStateToProps = ({ books }) => {
+  return {
+    books: books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
--- re-store/00-my-project/src/components/pages/home-page.js
+++ re-store/00-my-project/src/components/pages/home-page.js
@@ -2,19 +2,8 @@ import React from "react";
 import BookList from "../book-list";
 
 const HomePage = () => {
-  const books = [
-    { 
-      id: 1, 
-      title: 'Production ready microservices',
-      author: 'Susan J. Fowler'},
-    { 
-      id: 2, 
-      title: 'Release It!',
-      author: 'Michael T. Nygard'},
-  ]
-
   return (
-    <BookList books={books} />
+    <BookList />
   )
 }
 
