409c5efabffb8b2252b5d80883121a7fcf6fed78
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-11-14 20:23:18 +0300

    fin: 141. Обработка ошибок

--- re-store/00-my-project/src/components/book-list/book-list.js
+++ re-store/00-my-project/src/components/book-list/book-list.js
@@ -3,27 +3,36 @@ import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested } from '../../actions';
+import { booksLoaded, booksRequested, booksError } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner'
+import ErrorIndicator from '../error-indicator'
 
 import './book-list.css';
 
 class BookList extends Component {
 
   componentDidMount() {
-    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+    const { 
+      bookstoreService, 
+      booksLoaded, 
+      booksRequested, 
+      booksError } = this.props;
     booksRequested();
     bookstoreService.getBooks()
-      .then((data) => booksLoaded(data));
-
+      .then((data) => booksLoaded(data))
+      .catch((err) => booksError(err));
   }
 
   render() {
-    const { books, loading } = this.props;
+    const { books, loading, error } = this.props;
 
     if (loading) {
       return <Spinner />;
+    }
+
+    if (error) {
+      return <ErrorIndicator />
     }
 
     return (
@@ -40,13 +49,14 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({ books, loading }) => {
-  return { books, loading };
+const mapStateToProps = ({ books, loading, error }) => {
+  return { books, loading, error };
 };
 
 const mapDispatchToProps = {
   booksLoaded, 
-  booksRequested
+  booksRequested,
+  booksError
 };
 
 export default compose(
--- re-store/00-my-project/src/services/bookstore-service.js
+++ re-store/00-my-project/src/services/bookstore-service.js
@@ -17,8 +17,11 @@ export default class BookstoreService {
   ]
   
   getBooks() {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
+        if (Math.random() > 0.75) {
+          reject(new Error('Something bad happened'))
+        }
         resolve(this.data)
       }, 1000);
     });
